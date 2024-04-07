37d4a8fb8a7d5ff1f20029dc923216df14d69cbb
areka06 <116472942+areka06@users.noreply.github.com>
2024-04-07 12:31:08 +0700

    Add files via upload

---
+++ model.php
@@ -0,0 +1,8 @@
+<?php
+class contact   {
+    static function select() {}
+    static function view() {}
+    static function insert() {}
+    static function update() {}
+    static function delete() {}
+}
